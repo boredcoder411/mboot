c5df17d8b20aa46e6fd9c8a0c7f7e84640108c7f
boredcoder411 <taamarcos@gmail.com>
2025-11-03 19:32:58 +0100

    refactor: make the image script copy files directly

--- image.sh
+++ image.sh
@@ -7,6 +7,8 @@ DISK_SIZE_MB=64
 SECTOR_SIZE=512
 VOL_LABEL="DATA"
 
+FILES_TO_ADD=("$@")
+
 BOOT_SIZE_BYTES=$(stat -c%s "$BOOT_FILE" 2>/dev/null || stat -f%z "$BOOT_FILE")
 BOOT_SECTORS=$(( (BOOT_SIZE_BYTES + SECTOR_SIZE - 1) / SECTOR_SIZE ))
 DISK_SECTORS=$(( (DISK_SIZE_MB * 1024 * 1024) / SECTOR_SIZE ))
@@ -20,10 +22,8 @@ dd if=/dev/zero of="$DISK_IMG" bs=1M count="$DISK_SIZE_MB" status=progress
 echo "Writing boot.bin to first partition..."
 dd if="$BOOT_FILE" of="$DISK_IMG" bs=$SECTOR_SIZE conv=notrunc status=progress
 
-# --- Helper to output one byte in octal form ---
 byte() { printf "\\$(printf '%03o' "$1")"; }
 
-# --- Pack 32-bit little endian value ---
 le32() {
   local n=$1
   byte $(( n        & 0xFF ))
@@ -32,7 +32,6 @@ le32() {
   byte $(( (n>>24)  & 0xFF ))
 }
 
-# --- Partition layout ---
 P1_START=1
 P1_SIZE=$BOOT_SECTORS
 P2_START=$(( P1_START + P1_SIZE ))
@@ -42,10 +41,10 @@ write_partition() {
   local OFFSET=$1 START_LBA=$2 SIZE=$3 TYPE=$4
 
   {
-    byte 0x00                     # bootable flag
-    byte 0xFE; byte 0xFF; byte 0xFF   # CHS start
-    byte $TYPE                    # type
-    byte 0xFE; byte 0xFF; byte 0xFF   # CHS end
+    byte 0x00
+    byte 0xFE; byte 0xFF; byte 0xFF
+    byte $TYPE
+    byte 0xFE; byte 0xFF; byte 0xFF
     le32 $START_LBA
     le32 $SIZE
   } | dd of="$DISK_IMG" bs=1 seek=$OFFSET conv=notrunc status=none
@@ -58,6 +57,28 @@ printf '\x55\xAA' | dd of="$DISK_IMG" bs=1 seek=510 conv=notrunc status=none
 
 echo "Creating FAT16 filesystem on second partition..."
 mkfs.fat -F16 -n "$VOL_LABEL" --offset $P2_START "$DISK_IMG"
+
+if [ ${#FILES_TO_ADD[@]} -gt 0 ]; then
+  echo "Adding files to FAT16 partition using mtools..."
+
+  OFFSET_BYTES=$((P2_START * SECTOR_SIZE))
+  export MTOOLSRC=$(mktemp)
+  echo "drive z: file=\"$DISK_IMG\" offset=$OFFSET_BYTES" > "$MTOOLSRC"
+
+  for f in "${FILES_TO_ADD[@]}"; do
+    if [ -f "$f" ]; then
+      echo "Copying $f..."
+      mcopy -i "$DISK_IMG@@$OFFSET_BYTES" "$f" ::/
+    else
+      echo "Warning: file not found: $f"
+    fi
+  done
+
+  rm -f "$MTOOLSRC"
+  echo "Files added successfully!"
+else
+  echo "No files specified to add."
+fi
 
 echo "Disk image $DISK_IMG created successfully!"
 echo " - Partition 1: boot.bin ($BOOT_SECTORS sectors)"
