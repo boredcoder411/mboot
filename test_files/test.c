ff0aa360ec5432b6a8b0f4b47568503d26598625
boredcoder411 <taamarcos@gmail.com>
2025-11-04 10:43:03 +0100

    refactor: replace the syscall output test with outc

--- test_files/test.c
+++ test_files/test.c
@@ -1,5 +1,11 @@
+#include <stdint.h>
+
+void outb(uint16_t port, uint8_t value) {
+  asm volatile("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
 void _start() {
-  asm("int $0x80");
+  outb(0x3f8, 'a');
   while (1) {
   }
 }
