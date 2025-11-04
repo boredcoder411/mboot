a357dade457ee833f823bf493ac675ecdd7c1198
boredcoder411 <taamarcos@gmail.com>
2025-11-04 11:54:14 +0100

    moving files

--- stage2/cpu/interrupts/idt.asm
+++ stage2/cpu/interrupts/idt.asm
@@ -1,6 +1,27 @@
 [bits 32]
 [global idt_load]
 [global syscall_handler]
+[extern syscall_dispatch]
+
+%macro pusha_c 0
+    push eax
+    push ebx
+    push ecx
+    push edx
+    push ebp
+    push esi
+    push edi
+%endmacro
+
+%macro popa_c 0
+    pop edi
+    pop esi
+    pop ebp
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+%endmacro
 
 idt_load:
   mov eax, [esp + 4]
@@ -8,4 +29,19 @@ idt_load:
   ret
 
 syscall_handler:
-  iret
+    push 0
+    push 0
+    pusha_c
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    push esp
+    call syscall_dispatch
+    add esp, 4
+
+    popa_c
+    add esp, 8
+    iret
