05850018f918031762be283dac710322bad26299
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2026-07-19 00:36:00 -0400

    feat: implement core libc functions for freestanding environment

--- user/src/crt0.s
+++ user/src/crt0.s
@@ -1,10 +1,21 @@
 [bits 32]
 
 extern main
+extern exit
 
 section .text
 global _start
 
 _start:
+    ; Standard i386 ELF entry convention (Linux):
+    ;   [esp]   = argc
+    ;   [esp+4] = argv[0], argv[1], ..., NULL
+
+    mov eax, [esp]        ; argc
+    lea ebx, [esp+4]      ; argv = &argv[0] on stack
+
+    push ebx              ; argv
+    push eax              ; argc
     call main
-    jmp _start
+    push eax
+    call exit
